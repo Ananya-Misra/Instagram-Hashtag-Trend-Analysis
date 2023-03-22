c17b42c0414c8f9433ac89fcd0dfc537b30c9366
Ananya Misra <anumisra2@gmail.com>
2023-03-22 23:18:15 +0530

    visualization started

--- bot2.py
+++ bot2.py
@@ -2,14 +2,19 @@
 # !$@%67
 import pandas as pd
 from selenium import webdriver
+
 """ from webdriver_manager.chrome import ChromeDriverManager """
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import time
 from sqlalchemy import create_engine
+import sqlite3
+import pandas as pd
+import numpy as np
 
-#--------------------------------------------- **BOT 1---------------------------------------------------------**
+
+# --------------------------------------------- **BOT 1---------------------------------------------------------**
 
 # login
 def login(driver, instahandle, instapassword, sleeptime):
@@ -23,53 +28,59 @@ def login(driver, instahandle, instapassword, sleeptime):
     time.sleep(sleeptime)
     return True
 
-def skip_login_info(driver,sleeptime):
-    #save your login info?
+
+def skip_login_info(driver, sleeptime):
+    # save your login info?
     time.sleep(sleeptime)
     notnow = driver.find_element("xpath", "//button[contains(text(), 'Not Now')]").click()
-def turn_off_notif(driver,sleeptime):
-    #turn on notif
+
+
+def turn_off_notif(driver, sleeptime):
+    # turn on notif
     time.sleep(sleeptime)
     notnow2 = driver.find_element("xpath", "//button[contains(text(), 'Not Now')]").click()
 
-#searchbox
-def search(driver, instapage,sleeptime):
+
+# searchbox
+def search(driver, instapage, sleeptime):
     time.sleep(sleeptime)
     searchbox = driver.find_element(By.CSS_SELECTOR, "input[placeholder='Search']")
     searchbox.clear()
     searchbox.send_keys(instapage)
     time.sleep(sleeptime)
-    #selects the item
+    # selects the item
     searchbox.send_keys(Keys.ENTER)
     time.sleep(sleeptime)
-    #clicks enter
+    # clicks enter
     searchbox.send_keys(Keys.ENTER)
 
-def set_window_size(driver,sleeptime):
+
+def set_window_size(driver, sleeptime):
     time.sleep(sleeptime)
-    a=driver.get_window_size()
+    a = driver.get_window_size()
     driver.set_window_size(a["width"], a["height"])
 
-def scroller(driver,timeout,sleeptime):
-    #scroll
+
+def scroller(driver, timeout, sleeptime):
+    # scroll
     time.sleep(sleeptime)
     scrolldown = driver.execute_script(
-    "window.scrollTo(0, document.body.scrollHeight);var scrolldown=document.body.scrollHeight;return scrolldown;")
+        "window.scrollTo(0, document.body.scrollHeight);var scrolldown=document.body.scrollHeight;return scrolldown;")
     match = False
     while not match:
         last_count = scrolldown
         time.sleep(sleeptime)
         scrolldown = driver.execute_script(
-        "window.scrollTo(0, document.body.scrollHeight);var scrolldown=document.body.scrollHeight;return scrolldown;")
+            "window.scrollTo(0, document.body.scrollHeight);var scrolldown=document.body.scrollHeight;return scrolldown;")
         if last_count == scrolldown:
             match = True
         if timeout == 0:
             break
         else:
-            timeout-=1
+            timeout -= 1
 
 
-def insta_posts(driver,sleeptime, limit=10):
+def insta_posts(driver, sleeptime, limit=10):
     posts = []
     links = driver.find_elements(By.TAG_NAME, 'a')
     for link in links:
@@ -77,15 +88,16 @@ def insta_posts(driver,sleeptime, limit=10):
         print(link)
         print(post)
         print(limit)
-        if limit==0:
+        if limit == 0:
             break
         if '/p/' in post:
             posts.append(post)
-            limit-=1
+            limit -= 1
 
     return posts
 
-def getdata(driver,posts, sleeptime):
+
+def getdata(driver, posts, sleeptime):
     raw_data = []
     for post in posts:
         print(f"Visiting page {post}")
@@ -98,17 +110,17 @@ def getdata(driver,posts, sleeptime):
                 content = driver.find_elements(By.CSS_SELECTOR, selector)
                 for con in content:
                     html = con.get_attribute('innerHTML')
-                    with open('dummy_sec.html', 'w',encoding="utf-8") as f:
+                    with open('dummy_sec.html', 'w', encoding="utf-8") as f:
                         f.write(html)
-                    soup = BeautifulSoup(html,  'html.parser')
+                    soup = BeautifulSoup(html, 'html.parser')
 
                     for link in soup.find_all('a'):
                         raw_data.append({
-                            'tag':link.text,
-                            'link':link.attrs.get('href'),
-                            'page':soup.find('a').attrs.get('href'),
-                            'date':soup.find('time').attrs.get('datetime'),
-                            'img':soup.find('img').attrs.get('src')
+                            'tag': link.text,
+                            'link': link.attrs.get('href'),
+                            'page': soup.find('a').attrs.get('href'),
+                            'date': soup.find('time').attrs.get('datetime'),
+                            'img': soup.find('img').attrs.get('src')
                         })
                     print(raw_data)
             else:
@@ -122,14 +134,17 @@ def getdata(driver,posts, sleeptime):
 
 def covert_data_to_df(raw_data):
     df = pd.DataFrame(raw_data)
-    new_dataframe=df[df['tag'].str.startswith('#')]
-    sorted_df=new_dataframe.drop_duplicates().sort_values('tag')
+    new_dataframe = df[df['tag'].str.startswith('#')]
+    sorted_df = new_dataframe.drop_duplicates().sort_values('tag')
     return sorted_df
 
+
 def convert_df_to_csv(df):
-    df.to_csv('hastags_rawdata1.csv', index=None)
+    df.to_csv('hastags_rawdata1.csv', index=None, mode='a')
     print('Process Completed')
-#--------------------- **Calling functions*-------------------*
+
+
+# --------------------- **Calling functions*-------------------*
 #
 # #installing driver
 # mdriver = webdriver.Chrome(ChromeDriverManager().install())
@@ -168,34 +183,38 @@ def convert_df_to_csv(df):
 
 from datetime import datetime
 
-def save_dataframe(df:pd.DataFrame):
-     val = datetime.now().strftime("_%d_%m_%y")
-     print(val)
-     engine = create_engine('sqlite:///hashtags.sqlite3', echo=False)
-     df.to_sql(f'hashtag_{val}', con=engine, if_exists='append')
-     return val
 
-#Adding posts Col to DB
+def save_dataframe(df: pd.DataFrame):
+    val = datetime.now().strftime("_%d_%m_%y")
+    print(val)
+    engine = create_engine('sqlite:///hashtags.sqlite3', echo=False)
+    out = df.to_sql(f'hashtag_{val}', con=engine, if_exists='append')
+    print(f'total data added {out}')
+    return val
+
+
+# Adding posts Col to DB
 import sqlite3
+
+
 def insertColPost(tableName):
     conn = sqlite3.connect('hashtags.sqlite3')
     cur = conn.cursor()
-    doesExists= False
-    res=cur.execute(f"""PRAGMA table_info({tableName})""").fetchall()
+    doesExists = False
+    res = cur.execute(f"""PRAGMA table_info({tableName})""").fetchall()
     for x in range(len(res)):
-      if('posts' in res[x]):
-
-          doesExists=True
-          break
-    if(not doesExists):
-        query=f"""ALTER TABLE {tableName} ADD COLUMN posts INTEGER"""
+        if ('posts' in res[x]):
+            doesExists = True
+            break
+    if (not doesExists):
+        query = f"""ALTER TABLE {tableName} ADD COLUMN posts INTEGER"""
         cur.execute(query);
     conn.commit()
     conn.close()
 
 
 # going to each hashtag page and extracting the number of posts
-def tag_data(driver, row,sleeptime):
+def tag_data(driver, row, sleeptime):
     try:
         time.sleep(sleeptime)
         prefix = 'https://www.instagram.com/' + row[2]
@@ -205,14 +224,14 @@ def tag_data(driver, row,sleeptime):
         print(no_posts.text)
         return no_posts.text
     except Exception as e:
-            print(e)
+        print(e)
 
 
 def readSqliteTable(tableName, driver, sleeptime):
     sqliteConnection = sqlite3.connect('hashtags.sqlite3')
     cursor = sqliteConnection.cursor()
     print("Connected to SQLite")
-    counter=0
+    counter = 0
     sqlite_select_query = f"""SELECT * from {tableName} ORDER BY tag ASC"""
     cursor.execute(sqlite_select_query)
     records = cursor.fetchall()
@@ -223,7 +242,7 @@ def readSqliteTable(tableName, driver, sleeptime):
     for row in records:
 
         try:
-            val = tag_data(driver, row ,sleeptime+1)
+            val = tag_data(driver, row, sleeptime + 1)
             sql_update_query = f"""Update {tableName} set posts = '{val}' where link= '{row[2]}';"""
             allqueries += sql_update_query
             print(sql_update_query)
@@ -235,10 +254,24 @@ def readSqliteTable(tableName, driver, sleeptime):
     conn.commit()
     conn.close()
     print("Process Completed")
+
+
+def sqlToDf(tableName):
+    conn = sqlite3.connect('hashtags.sqlite3')
+    sql_query = pd.read_sql_query (f'''
+                                   SELECT * FROM {tableName}
+                                   ''', conn)
+    df = pd.DataFrame(sql_query, columns = ['tag', 'link','page','date','img', 'posts'])
+    df['date']= pd.to_datetime(df['date'])
+    df['posts'] = df['posts'].replace(',','',regex=True)
+    df = df.replace('None', np.nan).dropna()
+    df['posts'] = df['posts'].astype(float)
+    df=df.dropna()
+    return df
+
 # -------------------------***Calling Functions***
 
 # tableName=save_dataframe(dataframe)
 # mdriver = webdriver.Chrome(ChromeDriverManager().install())
 # insertColPost("hashtag_"+tableName)
 # readSqliteTable("hashtag_"+tableName,mdriver,3)
-
